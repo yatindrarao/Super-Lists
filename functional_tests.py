fe60d238a9e3b7e65739efad56cad198a7b7f770
Yatindra <yatindrarao.rao@gmail.com>
2017-01-11 11:11:28 +0530

    Adds unittest for more efficient tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
-import os
+import unittest
 
 chromedriver = "/usr/bin/chromedriver"
-browser = webdriver.Chrome(chromedriver)
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome(chromedriver)
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_and_retrieve_it_later(self):
+        # User first check out the homepage of to-do app
+        self.browser.get('http://localhost:8000')
+
+        # He notices browser title and header mention To-Do
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish Test!')
+
+if __name__ == '__main__':
+    unittest.main()
