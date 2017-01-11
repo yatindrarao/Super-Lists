e1a9a473d2a546746a20ad9b1554ebcae7ec5302
Yatindra <yatindrarao.rao@gmail.com>
2017-01-11 15:40:29 +0530

    Refactors functional test code

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_and_retrieve_it_later(self):
         # User first check out the homepage of to-do app
         self.browser.get('http://localhost:8000')
@@ -50,11 +55,9 @@ class NewVisitorTest(unittest.TestCase):
 
         # When he hits enter the page updates and now the page lists
         # should have 2 items
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy grocery items',[row.text for row in rows])
-        self.assertIn('2: Take vegetables', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy grocery items')
+        self.check_for_row_in_list_table('2: Take vegetables')
+        
         self.fail('Finish Test!')
 
 
