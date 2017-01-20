ebe69dcd49794e34c98a89b5007bd7d9fe0243e1
Yatindra <yatindrarao.rao@gmail.com>
2017-01-20 15:45:56 +0530

    Adjusts new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,7 +8,7 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error, "You can't have an empty list item")
+        self.assertEqual(error.text, "You can't have empty list item")
 
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk')
@@ -17,7 +17,7 @@ class ItemValidationTest(FunctionalTest):
 
         self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error, "You can't have an empty list item")
+        self.assertEqual(error.text, "You can't have empty list item")
 
         self.browser.find_element_by_id('id_new_item').send_keys('Buy tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,8 +1,10 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from lists.models import Item, List
 
 
 class ListAndItemModelTest(TestCase):
+
     def test_saving_and_retrieving_items(self):
         list_ = List()
         list_.save()
@@ -29,3 +31,10 @@ class ListAndItemModelTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item, second_item)
         self.assertEqual(second_saved_item.list, list_)
+
+    def test_cannot_save_empty_item(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
