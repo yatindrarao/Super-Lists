b0c6287e752089eb7cffcfda1554bb74de69fa01
Yatindra <yatindrarao.rao@gmail.com>
2017-01-20 14:43:26 +0530

    Adds empty item validation FT

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,4 +4,21 @@ from unittest import skip
 class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_items(self):
-        pass
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error, "You can't have an empty list item")
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error, "You can't have an empty list item")
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Buy tea')
