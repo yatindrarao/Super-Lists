75d315d21e356ffd7886096f297af7b01dd4cf7c
Yatindra <yatindrarao.rao@gmail.com>
2017-01-20 16:31:15 +0530

    Validation in list view

--- lists/views.py
+++ lists/views.py
@@ -8,10 +8,16 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id= list_id)
+    error = None
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect('/lists/%d/' % (list_.id,))
-    return render(request, 'lists.html', {'list': list_})
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect('/lists/%d/' % (list_.id,))
+        except ValidationError:
+            error = "You can't have empty list item"
+    return render(request, 'lists.html', {'list': list_, 'error': error})
 
 def new_list(request):
     list_ = List.objects.create()
