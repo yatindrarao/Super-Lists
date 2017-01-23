5585ce0d4a092a3dea861a733d586129834e6c81
Yatindra <yatindrarao.rao@gmail.com>
2017-01-23 11:00:06 +0530

    Refactor get item call and replacing name of form in lists

--- lists/views.py
+++ lists/views.py
@@ -12,17 +12,19 @@ def view_list(request, list_id):
     error = None
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
         except ValidationError:
+            list_.delete()
             error = "You can't have empty list item"
+            return render(request, 'home.html', {"error": error})
     return render(request, 'lists.html', {'list': list_, 'error': error})
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
