ed5a6165b07e6f550f9c85717991016bbab98081
Yatindra <yatindrarao.rao@gmail.com>
2017-01-25 11:34:10 +0530

    changes save logic to form.save() method

--- lists/forms.py
+++ lists/forms.py
@@ -4,6 +4,11 @@ from lists.models import Item
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+
     class Meta:
         model = Item
         fields = ('text',)
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.models import List, Item
+
 
 class ItemFormTest(TestCase):
 
@@ -12,3 +14,11 @@ class ItemFormTest(TestCase):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do this work'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do this work')
+        self.assertEqual(new_item.list, list_)
